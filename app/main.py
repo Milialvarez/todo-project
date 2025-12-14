beb114fdc35f6e8271b30e5acaf5d427e1c88c8e
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-12-13 22:36:42 -0300

    add corsmiddleware config to allow comunication between frontend and backend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import auth, tasks, users
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",  # Next.js
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
