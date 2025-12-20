a21ef5982d796d1a3cbfae31b3f592f3347904f7
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-12-19 22:19:21 -0300

    add my first ci file config to try github actions

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -20,4 +20,4 @@ class TaskRead(BaseModel):
     status: StatusEnum
 
     class Config:
-        orm_mode = True
+        from_attributes = True
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -12,7 +12,7 @@ class UserRead(BaseModel):
     is_active: bool
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class UserLogin(BaseModel):
     email: EmailStr
