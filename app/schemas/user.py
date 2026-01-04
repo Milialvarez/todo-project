51dc88e567c603b2f88210d80b21ac6770ab4f02
milagros alvarez <milagrosalvarez2604@gmail.com>
2026-01-04 10:44:51 -0300

    refactor(user schema): add fied validator for the user create schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,9 +1,25 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 class UserCreate(BaseModel):
-    username: str
-    email: EmailStr
-    password: str
+    username: str 
+    email: EmailStr = Field(
+        ...,
+        description="user email, by this field the user would log in the app",
+        example="milagrosalvarez2604@gmail.com"
+    )
+    password: str = Field(
+        ...,
+        description="user password, also used to log in the app",
+        min_length=6,
+        example="123456"
+    )
+
+    @field_validator("password")
+    @classmethod
+    def password_min_lenght(cls, value:str):
+        if value.__len__ < 6:
+            raise ValueError("The password should have at least 6 characters")
+        return value
 
 class UserRead(BaseModel):
     id: int
